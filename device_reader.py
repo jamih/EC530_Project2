0af80c5afac704cd3969d066e666bdbdbf768e9c
Jami <jamih@bu.edu>
2022-02-14 11:19:33 -0500

    added device json parser file

---
+++ device_reader.py
@@ -0,0 +1,17 @@
+# Python program to read device json file
+
+import json
+# opening json file
+f = open('device_example.json')
+
+# returns JSON object as a dictionary
+data = json.load(f)
+
+# iterating through json list
+
+for i in data:
+    print(i)
+
+
+# closing file
+f.close()
