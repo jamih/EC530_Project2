44b2ab56eb095660ddc5d45acd858ee2829a78b4
Jami <jamih@bu.edu>
2022-02-15 10:26:06 -0500

    added functions

--- device_reader.py
+++ device_reader.py
@@ -2,16 +2,26 @@
 
 import json
 # opening json file
-f = open('device_example.json')
 
-# returns JSON object as a dictionary
-data = json.load(f)
+def json_check():
+    f = open('device_example.json')
 
-# iterating through json list
+    # returns JSON object as a dictionary
+    data = json.load(f)
 
-for i in data['devices']:
-    print(i)
+    # iterating through json list
+
+    for i in data:
+        print(i)
 
 
-# closing file
-f.close()
+    # closing file
+    f.close()
+
+
+def main():
+    json_check()
+
+
+if __name__ == '__main__':
+    main()
