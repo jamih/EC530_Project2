278ff4a447d5558fa0b1399ffe2bf88c9d7e8ad6
Jami <jamih@bu.edu>
2022-02-17 08:26:01 -0500

    added json validation

--- device_reader.py
+++ device_reader.py
@@ -3,24 +3,50 @@
 import json
 # opening json file
 
-def json_check():
+
+
+# first check that what's loaded from the file or string is 
+# a valid json file
+# then save the json file to a dict
+# check the fields of the dict
+# check if it's a string
+# check for negative measurements
+# check for format for bp
+# check that it's a valid string with numbers or letters
+def json_read():
+    
+    valid = json_validate()
+    print(valid)
+    if(valid):
+        f = open('device_example.json')
+        # returns JSON object as a dictionary
+        data = json.load(f)
+        print(data)
+    else:
+        exit()
+
+   
+    #return data
+
+def json_validate():
     f = open('device_example.json')
+    
+    # throw ValueError if string or data passed can't be
+    # decoded as json
+    try: 
+        json.load(f)
+    except ValueError as err:
+        return False
+    return True
 
-    # returns JSON object as a dictionary
-    data = json.load(f)
-
-    # iterating through json list
-
-    for i in data:
-        print(i)
-
-
-    # closing file
+     # closing file
     f.close()
 
 
 def main():
-    json_check()
+    #print(json_read())
+    json_read()
+    #json_validate()
 
 
 if __name__ == '__main__':
