ad7813c992ba36581e064b12e75a6de9f6590d04
Jami <jamih@bu.edu>
2022-02-24 08:31:17 -0500

    testing out flask api

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+from pickle import APPENDS
 from flask import Flask
+from flask_restful import Api, Resource
+
 app = Flask(__name__)
-@app.route('/hello/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World!"
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self, name):
+        return {"data": "Hello World"}
+    def post(self):
+        return {"data": "posted"}
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+    app.run(debug=True)
