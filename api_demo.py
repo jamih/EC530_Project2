46420ece2e041f999ff03d202fc6d6c5f79bb13e
Jami <jamih@bu.edu>
2022-03-02 22:11:21 -0500

    changing path of deployment

--- api_demo.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-BASE = "http://127.0.0.1:5000/"
-
-# response = requests.get(BASE + "helloworld/bill")
-response = requests.post(BASE + "video/1", {"likes:10"})
-print(response.json())
--- app.py
+++ app.py
@@ -1,30 +1,75 @@
-from pickle import APPENDS
-from flask import Flask
-from flask_restful import Api, Resource
+import flask
+from flask import request, jsonify
 
-app = Flask(__name__)
-api = Api(app)
-
-# names = {"tim": {"age": 19, "gender": "male"},
-#  "bill": {"age": 70, "gender": "male"}}
-
-videos = {}
-
-# class HelloWorld(Resource):
-#     def get(self, name):
-#         return names[name]
-#     # def post(self):
-#     #     return {"data": "posted"}
-
-class Video(Resource):
-    def get(self, video_id):
-        return videos[video_id]
-    def post(self, video_id):
-        return
-
-# api.add_resource(HelloWorld, "/helloworld/<string:name>")
-api.add_resource(Video, "/video/<int:video_id>")
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+devices = [
+    {
+    "Device_ID": 1,
+    "Device_Name": "NEW DEVICE",
+    "Type_of_Measurement": "blood pressure",
+    "Measurement": {
+        "Unit": "lb",
+        "Value": 70
+    },
+    "Patient_ID": 1
+    
+    },
+    {
+    "Device_ID": 2,
+    "Device_Name": "Glucometer",
+    "Type_of_Measurement": "glucose content",
+    "Measurement": {
+        "Unit": "mg/dL",
+        "Value": 100
+    },
+    "Patient_ID": 1
+    
+    },
+
+    {
+    "Device_ID": 3,
+    "Device_Name": "Scale",
+    "Type_of_Measurement": "weight",
+    "Measurement": {
+        "Unit": "lb",
+        "Value": 130.2
+    },
+    "Patient_ID": 1
+    
+}
+
+]
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Devices API</h1>
+<p>/devices/all will return all devices</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/devices/all', methods=['GET'])
+def api_all():
+    return jsonify(devices)
+
+@app.route('/devices/post', methods=['POST'])
+def post_device():
+    content_type = request.headers.get('Content-Type')
+    # check if the file sent is in json format
+    if (content_type == 'application/json'):
+        json = request.get_json()
+        #json_device = json["device"]
+        #return json_device
+        return json
+    else:
+        return 'Content-Type not supported!'
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
